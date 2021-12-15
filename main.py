df0cb41c94897956c8538969ac6c2087befb80fe
Tommy1992 <29047092+Tommy1992@users.noreply.github.com>
2021-12-15 22:36:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+#Step 3
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for letter in range(word_length):
+    display += "_"
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+
+
+#Check guessed letter
+counter=0
+
+while "_" in display:
+  guess = input("Guess a letter: ").lower()
+  counter += 1
+  for position in range(word_length):
+    letter = chosen_word[position]
+    #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == guess:
+      display[position] = letter
+      
+      print(display)
+
+
+
+print (f"You won. You needed: {counter} guesses.")
